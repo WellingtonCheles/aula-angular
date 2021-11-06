eb2c4382a57f15286c3b2ba480031fdc16faef1c
WellingtonCheles <wellingtoncheles@gmail.com>
2021-11-06 14:05:41 -0300

    angular-parte2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { Page401Component } from './page401/page401.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: '', component:LoginComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'cadastro', component:CadastroComponent},
+  {path: '**', component:Page401Component
+  
+
+  }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule,RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,93 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { CommonModule } from '@angular/common';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+
+import { HomeComponent } from './home/home.component';
+
+import { Page401Component } from './page401/page401.component';
+
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+
+
+
+import { AngularFireModule } from '@angular/fire';
+
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { AuthService } from './auth.service';
+import { CadastroAdmComponent } from './cadastro-adm/cadastro-adm.component';
+
+
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+declarations: [
+
+AppComponent,
+
+LoginComponent,
+
+HomeComponent,
+
+Page401Component,
+
+CadastroComponent,
+ CadastroAdmComponent,
+
+],
+
+imports: [
+
+CommonModule,
+
+FormsModule,
+
+BrowserModule,
+
+AppRoutingModule,
+
+HttpClientModule,
+
+ReactiveFormsModule,
+
+AngularFireModule.initializeApp(
+
+{
+
+  apiKey: "AIzaSyDH3N0tF4QBEG8sGyUGOwsa210P8PX3aHE",
+  authDomain: "conversor-angular-cast.firebaseapp.com",
+  projectId: "conversor-angular-cast",
+  storageBucket: "conversor-angular-cast.appspot.com",
+  messagingSenderId: "1086916266338",
+  appId: "1:1086916266338:web:351bba4d2aeebe5af1443b"
+
+}
+
+), AngularFireAuthModule
+
+],
+
+providers: [AuthService],
+
+bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
+
